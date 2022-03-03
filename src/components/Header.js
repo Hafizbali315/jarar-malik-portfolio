3537941e6a242ddc4377412b12783878f35cf7db
hafizbali315 <hafizbali315@gmail.com>
2022-03-03 16:10:16 +0500

    Navbar

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import Navbar from './Navbar'
+
+const Header = () => {
+	return (
+		<div className="header">
+			<Navbar />
+
+			<div className="hero">Hero</div>
+		</div>
+	)
+}
+
+export default Header
---
+++ src/components/Navbar.js
@@ -0,0 +1,142 @@
+import { useState } from 'react'
+
+import LogoWhite from '../assets/LogoWhite.png'
+
+import { FaDribbbleSquare, FaBehanceSquare, FaTelegramPlane, FaInstagram } from 'react-icons/fa'
+import { BsDiscord } from 'react-icons/bs'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { MdCancel } from 'react-icons/md'
+
+const Navbar = () => {
+	const [showSidebar, setShowSidebar] = useState()
+
+	return (
+		<div className="navbar">
+			<div className="container">
+				<div className="content">
+					<div className="logo">
+						<img src={LogoWhite} alt="logo" />
+					</div>
+
+					<ul className="page_links">
+						<li>
+							<a href="#about">about</a>
+						</li>
+						<li>
+							<a href="#projects">projects</a>
+						</li>
+						<li>
+							<a href="#clients">clients</a>
+						</li>
+						<li>
+							<a href="#contact">contact</a>
+						</li>
+					</ul>
+
+					<ul className="social_links">
+						<li>
+							<a href="#dribble">
+								<FaDribbbleSquare className="icon" />
+							</a>
+						</li>
+
+						<li>
+							<a href="#behance">
+								<FaBehanceSquare className="icon" />
+							</a>
+						</li>
+
+						<li>
+							<a href="#instagram">
+								<FaInstagram className="icon" />
+							</a>
+						</li>
+
+						<li>
+							<a href="#telegram">
+								<FaTelegramPlane className="icon" />
+							</a>
+						</li>
+
+						<li>
+							<a href="#discord">
+								<BsDiscord className="icon" />
+							</a>
+						</li>
+					</ul>
+
+					<div className="hamburger_menu">
+						{showSidebar ? (
+							<span onClick={() => setShowSidebar(false)}> X </span>
+						) : (
+							<GiHamburgerMenu className="icon" onClick={() => setShowSidebar(true)} />
+						)}
+					</div>
+
+					{showSidebar && (
+						<div className="mobile_sidebar">
+							<ul className="mb_links">
+								<li>
+									<a href="#about" onClick={() => setShowSidebar(false)}>
+										about
+									</a>
+								</li>
+								<li>
+									<a href="#projects" onClick={() => setShowSidebar(false)}>
+										projects
+									</a>
+								</li>
+								<li>
+									<a href="#clients" onClick={() => setShowSidebar(false)}>
+										clients
+									</a>
+								</li>
+								<li>
+									<a href="#contact" onClick={() => setShowSidebar(false)}>
+										contact
+									</a>
+								</li>
+
+								<li>
+									<ul className="mb_social_links">
+										<li>
+											<a href="#dribble">
+												<FaDribbbleSquare className="icon" />
+											</a>
+										</li>
+
+										<li>
+											<a href="#behance">
+												<FaBehanceSquare className="icon" />
+											</a>
+										</li>
+
+										<li>
+											<a href="#instagram">
+												<FaInstagram className="icon" />
+											</a>
+										</li>
+
+										<li>
+											<a href="#telegram">
+												<FaTelegramPlane className="icon" />
+											</a>
+										</li>
+
+										<li>
+											<a href="#discord">
+												<BsDiscord className="icon" />
+											</a>
+										</li>
+									</ul>
+								</li>
+							</ul>
+						</div>
+					)}
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Navbar
