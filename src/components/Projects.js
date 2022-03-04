dd5570327de4096f1ee921357a00d793a5c45e6e
hafizbali315 <hafizbali315@gmail.com>
2022-03-04 07:40:17 +0500

    Projects Popup

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,51 @@
+import { useEffect, useState } from 'react'
+
 import EularImg from '../assets/EularImg.png'
 import EularCardBg from '../assets/EularCardBg.png'
 import EularHoverImg from '../assets/EularHoverImg.png'
+import ProjectImg from '../assets/ProjectImg.png'
 
 const Projects = () => {
+	const [showProjectDetails, setShowProjectDetails] = useState(false)
+	const [projectDetails, setProjectDetails] = useState()
+
+	useEffect(() => {
+		console.log(projectDetails)
+	}, [projectDetails])
+
+	const ProjectsDetails = [
+		{
+			id: 0,
+			name: 'Project Name',
+			tag: 'UI/UX Website App Design',
+			link: 'Euler.finance',
+			hoverImg: EularHoverImg,
+			detail:
+				'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
+			img: ProjectImg,
+		},
+		{
+			id: 1,
+			name: 'Project Name',
+			tag: 'UI/UX Website App Design',
+			link: 'Euler.finance',
+			hoverImg: EularHoverImg,
+			detail:
+				'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
+			img: ProjectImg,
+		},
+		{
+			id: 2,
+			name: 'Project Name',
+			tag: 'UI/UX Website App Design',
+			link: 'Euler.finance',
+			hoverImg: EularHoverImg,
+			detail:
+				'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
+			img: ProjectImg,
+		},
+	]
+
 	return (
 		<div className="projects" id="projects">
 			<div className="container">
@@ -10,44 +53,44 @@ const Projects = () => {
 
 				<div className="content">
 					<div className="projects_row">
-						<div className="project">
-							<img className="eular_img" src={EularImg} alt="EularImage" />
+						{ProjectsDetails.map((project) => (
+							<div
+								className="project"
+								key={project.id}
+								onClick={() => {
+									setShowProjectDetails(true)
+									setProjectDetails(project)
+								}}
+							>
+								<img className="eular_img" src={EularImg} alt="EularImage" />
 
-							<img className="eular_card_bg" src={EularCardBg} alt="EularCardBg" />
-							<img className="eular_hover_img" src={EularHoverImg} alt="EularHoverImg" />
+								<img className="eular_card_bg" src={EularCardBg} alt="EularCardBg" />
+								<img className="eular_hover_img" src={EularHoverImg} alt="EularHoverImg" />
 
-							<div className="eular_content">
-								<h6>Euler.finance</h6>
-								<p>UI/UX Website App Design</p>
+								<div className="eular_content">
+									<h6>{project.link}</h6>
+									<p>{project.tag}</p>
+								</div>
 							</div>
-						</div>
-
-						<div className="project">
-							<img className="eular_img" src={EularImg} alt="EularImage" />
-
-							<img className="eular_card_bg" src={EularCardBg} alt="EularCardBg" />
-							<img className="eular_hover_img" src={EularHoverImg} alt="EularHoverImg" />
-
-							<div className="eular_content">
-								<h6>Euler.finance</h6>
-								<p>UI/UX Website App Design</p>
-							</div>
-						</div>
-
-						<div className="project">
-							<img className="eular_img" src={EularImg} alt="EularImage" />
-
-							<img className="eular_card_bg" src={EularCardBg} alt="EularCardBg" />
-							<img className="eular_hover_img" src={EularHoverImg} alt="EularHoverImg" />
-
-							<div className="eular_content">
-								<h6>Euler.finance</h6>
-								<p>UI/UX Website App Design</p>
-							</div>
-						</div>
+						))}
 					</div>
 				</div>
 			</div>
+
+			{showProjectDetails && projectDetails && (
+				<div className="popup">
+					<div className="container">
+						<div className="project_details">
+							<h1>{projectDetails.name}</h1>
+							<p>{projectDetails.detail}</p>
+
+							<img src={projectDetails.img} alt="ProjectImg" />
+
+							<span onClick={() => setProjectDetails(false)}> X </span>
+						</div>
+					</div>
+				</div>
+			)}
 		</div>
 	)
 }
