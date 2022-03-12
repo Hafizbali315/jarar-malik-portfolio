e2129b315166a11abe7cafa5e2d505beb568f22a
hafizbali315 <hafizbali315@gmail.com>
2022-03-12 21:44:28 +0500

    Navbar Fixed (v4)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,6 @@
 import { useState } from 'react'
 
 import LogoWhite from '../assets/LogoWhite.png'
-
-import { FaLinkedinIn, FaBehanceSquare, FaTelegramPlane, FaInstagram, FaTwitter } from 'react-icons/fa'
-import { BsDiscord } from 'react-icons/bs'
 import { GiHamburgerMenu } from 'react-icons/gi'
 
 const Navbar = () => {
@@ -13,11 +10,13 @@ const Navbar = () => {
 		<div className="navbar">
 			<div className="container">
 				<div className="content">
-					<div className="logo">
-						<img src={LogoWhite} alt="logo" />
-					</div>
+					<ul className="lg_page_links">
+						<li className="logo">
+							<a href="#top">
+								<img src={LogoWhite} alt="logo" />
+							</a>
+						</li>
 
-					<ul className="page_links">
 						<li>
 							<a href="#about">about</a>
 						</li>
@@ -30,42 +29,10 @@ const Navbar = () => {
 						<li>
 							<a href="#contact">contact</a>
 						</li>
-					</ul>
-
-					<ul className="social_links">
-						<li>
-							<a href="https://www.linkedin.com/in/jarar-nazar/" target="_blank" rel="noreferrer">
-								<FaLinkedinIn className="icon" />
-							</a>
-						</li>
 
 						<li>
 							<a href="https://www.behance.net/Jararjerry51ba" target="_blank" rel="noreferrer">
-								<FaBehanceSquare className="icon" />
-							</a>
-						</li>
-
-						<li>
-							<a href="https://www.instagram.com/jararmalik95/" target="_blank" rel="noreferrer">
-								<FaInstagram className="icon" />
-							</a>
-						</li>
-
-						<li>
-							<a href="https://telegram.me/JayForeJessi" target="_blank" rel="noreferrer">
-								<FaTelegramPlane className="icon" />
-							</a>
-						</li>
-
-						<li>
-							<a href="https://discord.com/channels/808234830579499009" target="_blank" rel="noreferrer">
-								<BsDiscord className="icon" />
-							</a>
-						</li>
-
-						<li>
-							<a href="https://twitter.com/JararMalik" target="_blank" rel="noreferrer">
-								<FaTwitter className="icon" />
+								behance
 							</a>
 						</li>
 					</ul>
@@ -103,43 +70,9 @@ const Navbar = () => {
 								</li>
 
 								<li>
-									<ul className="mb_social_links">
-										<li>
-											<a href="https://www.linkedin.com/in/jarar-nazar/" target="_blank" rel="noreferrer">
-												<FaLinkedinIn className="icon" />
-											</a>
-										</li>
-
-										<li>
-											<a href="https://www.behance.net/Jararjerry51ba" target="_blank" rel="noreferrer">
-												<FaBehanceSquare className="icon" />
-											</a>
-										</li>
-
-										<li>
-											<a href="https://www.instagram.com/jararmalik95/" target="_blank" rel="noreferrer">
-												<FaInstagram className="icon" />
-											</a>
-										</li>
-
-										<li>
-											<a href="https://telegram.me/JayForeJessi" target="_blank" rel="noreferrer">
-												<FaTelegramPlane className="icon" />
-											</a>
-										</li>
-
-										<li>
-											<a href="https://discord.com/channels/808234830579499009" target="_blank" rel="noreferrer">
-												<BsDiscord className="icon" />
-											</a>
-										</li>
-
-										<li>
-											<a href="https://twitter.com/JararMalik" target="_blank" rel="noreferrer">
-												<FaTwitter className="icon" />
-											</a>
-										</li>
-									</ul>
+									<a href="https://www.behance.net/Jararjerry51ba" onClick={() => setShowSidebar(false)} target="_blank" rel="noreferrer">
+										behance
+									</a>
 								</li>
 							</ul>
 						</div>
