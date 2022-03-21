4c655950f81163d432ad108eeb1899c671416a85
hafizbali315 <hafizbali315@gmail.com>
2022-03-21 07:36:02 +0500

    Noice gif added

--- src/App.js
+++ src/App.js
@@ -5,14 +5,22 @@ import Contact from './components/Contact'
 import Footer from './components/Footer'
 import Projects from './components/Projects'
 
+// import Noice from './assets/noise.gif'
+
 function App() {
 	return (
 		<div className="App">
-			<Header />
-			<About />
-			<Projects />
-			<Clients />
-			<Contact />
+			<div className="sections">
+				<div className="bg"></div>
+
+				<div className="content">
+					<Header />
+					<About />
+					<Projects />
+					<Clients />
+					<Contact />
+				</div>
+			</div>
 			<Footer />
 		</div>
 	)
